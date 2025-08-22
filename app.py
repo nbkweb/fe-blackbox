2f45c874d0f6d5c192ac8b3387bbdabdb9a36477
nbkweb <nbkwebprojects@gmail.com>
2025-08-22 21:27:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ from config import config
 csrf = CSRFProtect()
 
 def create_app(config_name='default'):
-    app = Flask(__name__)
+    # Point Flask to look for templates and static files in the app folder
+    app = Flask(__name__, 
+                template_folder='app/templates',
+                static_folder='app/static')
     app.config.from_object(config[config_name])
     
     csrf.init_app(app)
     
-    from routes import main as main_blueprint
+    # Import from the app folder
+    from app.routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
     return app
