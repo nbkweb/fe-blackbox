7bff704bc2ed4795c2f0c5fcf1244ea1b04e692d
nbkweb <nbkwebprojects@gmail.com>
2025-08-22 21:00:54 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ def create_app(config_name='default'):
     
     csrf.init_app(app)
     
-    from .routes import main as main_blueprint
+    from routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
     return app
