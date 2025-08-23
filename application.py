4f460e863e4e943fa32600d8fb9392fac5e0fd77
nbkweb <nbkwebprojects@gmail.com>
2025-08-23 14:45:30 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
-from flask_login import LoginManager
 from config import config
-from app.models import db, Merchant
 
 csrf = CSRFProtect()
 
@@ -15,22 +13,9 @@ def create_app(config_name='default'):
     
     app.config.from_object(config[config_name])
     csrf.init_app(app)
-    db.init_app(app)
 
-    # Setup Flask-Login
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return Merchant.query.get(int(user_id))
-
-    # Register blueprints
+    # Register only frontend-related blueprints
     from app.routes import main as main_blueprint
-    from app.auth import auth as auth_blueprint
-
-    app.register_blueprint(auth_blueprint)
     app.register_blueprint(main_blueprint)
 
     return app
